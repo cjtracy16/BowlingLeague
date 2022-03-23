61b5c59228fbd1c62ab2fac11ac7e8565a18b915
cjtracy <connertracy@comcast.net>
2022-03-23 09:41:01 -0600

    Created App set up Context and Connection String

---
+++ BowlingLeague/BowlingLeague/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using BowlingLeague.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BowlingLeague.Controllers
+{
+    public class HomeController : Controller
+    {
+        public HomeController()
+        {
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
